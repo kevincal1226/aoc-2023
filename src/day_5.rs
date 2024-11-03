9d05cab6b3f3f40b3c71aa3263f9cb3c1db41260
Kevin Calopisis <kevincal@umich.edu>
2024-11-03 13:29:59 -0500

    day 5 STARTED

---
+++ src/day_5.rs
@@ -0,0 +1,10 @@
+use std::io::BufRead;
+
+pub fn day5_part1() -> u64 {
+    let reader = std::io::BufReader::new(std::fs::File::open("day-5.txt").unwrap());
+    reader.lines().for_each(|line| {});
+    0
+}
+pub fn day5_part2() -> u64 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day_24;
 mod day_25;
 mod day_3;
 mod day_4;
+mod day_5;
 
 fn main() {
     //println!("Day 1 output: {}", day_1::day1());
@@ -12,6 +13,8 @@ fn main() {
     //println!("Day 3 output: {}", day_3::day3_part2());
     //println!("Day 24 output: {}", day_24::day24_part2());
     //println!("Day 23 output: {}", day_23::day23_part2());
-    println!("Day 4 output: {}", day_4::day4_part1_cursed());
-    println!("Day 4 output: {}", day_4::day4_part2());
+    //println!("Day 4 output: {}", day_4::day4_part1_cursed());
+    //println!("Day 4 output: {}", day_4::day4_part2());
+    println!("Day 5 output: {}", day_5::day5_part1());
+    println!("Day 5 output: {}", day_5::day5_part2());
 }
